14d1f4224123d49bcc4cefbadd98e620a26c01df
Fatheed7 <69724750+Fatheed7@users.noreply.github.com>
2022-12-14 20:06:48 +0000

    Adjusted settings.py - ALLOWED HOSTS

--- pyramid/settings.py
+++ pyramid/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['pyramid-heroku.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pyramid-ultimate.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
